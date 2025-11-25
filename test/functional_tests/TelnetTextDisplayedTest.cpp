8674ba098ff8642cd37a1cc36ced06fb213fa936
Nicolas KEITA <nicolas.keita@epitech.eu>
2025-11-25 10:32:15 +0100

    test

--- test/functional_tests/TelnetTextDisplayedTest.cpp
+++ test/functional_tests/TelnetTextDisplayedTest.cpp
@@ -39,7 +39,8 @@ private:
     TelnetServerStub* mpServer = nullptr;
     const QString mpHostname = "Test-Telnet";
     const QString mpPort = "5559";
-    const QString mpLocalhost = "127.0.0.1";
+    //const QString mpLocalhost = "127.0.0.1";
+    const QString mpLocalhost = "0.0.0.0";
     //const QString mpLocalhost = "::1";
 
 private slots:
@@ -93,7 +94,8 @@ private slots:
             QTest::qWait(100);
             QTest::keyClick(QApplication::focusWidget(), Qt::Key_Tab);
             QTest::qWait(100);
-            QTest::keyClicks(QApplication::focusWidget(), address);
+            //QTest::keyClicks(QApplication::focusWidget(), address);
+            QTest::keyClicks(QApplication::focusWidget(), "127.0.0.1");
             QTest::qWait(100);
             QTest::keyClick(QApplication::focusWidget(), Qt::Key_Tab);
             QTest::qWait(100);
