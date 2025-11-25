bbf95cebcc7f53387495f6fcbef013f594f58559
Nicolas KEITA <nicolas.keita@epitech.eu>
2025-11-25 09:14:54 +0100

    test

--- src/ctelnet.cpp
+++ src/ctelnet.cpp
@@ -1238,7 +1238,8 @@ connect(&mSocket_ipV4, &QAbstractSocket::errorOccurred,
         });
 
 QTcpSocket testSock;
-testSock.connectToHost("127.0.0.1", mHostPort, QIODevice::ReadWrite, QAbstractSocket::IPv4Protocol);
+//testSock.connectToHost("127.0.0.1", mHostPort, QIODevice::ReadWrite, QAbstractSocket::IPv4Protocol);
+testSock.connectToHost(hostInfo.hostName(), mHostPort, QIODevice::ReadWrite, QAbstractSocket::IPv4Protocol);
 if (!testSock.waitForConnected(1000)) {
     qCritical() << "Failed to connect to stub:" << testSock.errorString();
 } else {
